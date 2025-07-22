2ca8476c67a7af96b745eb9b60b10d449bae5f72
Nic Pottier <nicpottier@gmail.com>
2025-07-22 09:09:00 -0700

    types on async methods

--- adt_press/utils/sync.py
+++ adt_press/utils/sync.py
@@ -1,9 +1,12 @@
 import asyncio
+from typing import Awaitable, Callable, List, Never, TypeVar
 
 from asynciolimiter import Limiter
 
+T = TypeVar("T")
 
-def run_async_task(task):
+
+def run_async_task(task: Callable[[], Awaitable[T]]) -> T:
     """
     Run an async task in a synchronous context."""
     loop = asyncio.new_event_loop()
@@ -15,7 +18,7 @@ def run_async_task(task):
     return results
 
 
-def gather_with_limit(fs, rate_limit):
+def gather_with_limit(fs: List[Awaitable[Never]], rate_limit: int) -> Awaitable[List[T]]:
     """Gather async tasks with a rate limit."""
     limiter = Limiter(rate_limit / 60)
     return asyncio.gather(*[limiter.wrap(f) for f in fs])
