13f8dffa4c18b507b52e7629f61b4a34fd88f08e
Nic Pottier <nicpottier@gmail.com>
2025-08-25 10:13:09 -0700

    add default_model config option for easier switching

--- adt_press/utils/config.py
+++ adt_press/utils/config.py
@@ -6,3 +6,8 @@ from omegaconf import DictConfig, ListConfig, OmegaConf
 
 def conf_to_object(value: DictConfig | ListConfig) -> dict[str | bytes | int | Enum | float | bool, Any] | list[Any] | str | Any | None:
     return {} if value is None else OmegaConf.to_container(value, resolve=True)
+
+def prompt_config_with_model(value: DictConfig, default_model: str) -> DictConfig:
+    if value["model"] == "default":
+        value["model"] = default_model
+    return conf_to_object(value)
