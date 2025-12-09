88fdecf20ec6b8bbcf06e4c78e4e65120b84e846
Katherine <khof312@gmail.com>
2025-12-09 11:14:44 +0100

    Move stripping of whitespace from normalizer to standardization routine.

--- adt_eval/utils/transcript_cleaner.py
+++ adt_eval/utils/transcript_cleaner.py
@@ -2,7 +2,7 @@ import re
 
 def normalize_transcript(text:str) -> str:
     """
-    Normalizes a transcript by removing punctuation, special characters, and excess space, as well as and converting to lowercase.
+    Normalizes a transcript by removing punctuation/special characters and converting to lowercase.
     This normalization is not useful for producing an ADT, but rather only for matching content between the Gold Standard and the raw LLM output.
     """
     
@@ -13,16 +13,17 @@ def normalize_transcript(text:str) -> str:
     # Remove punctuation and special characters
     text = re.sub(r'[^\w\s]', '', text)
 
-    # Remove excess whitespace
-    text = re.sub(r'\s+', ' ', text).strip()
-
     return text
 
 
-def standardize_transcript(text:str) -> str:
+def standardize_transcript(text:str, strip_whitespace:bool=True) -> str:
     """
     Apply standard cleaning steps to the transcript text.
     This could be useful for producing an ADT, but is not currently applied in the core ADT pipeline.
+
+    Args:
+        text (str): The transcript text to standardize.
+        strip_whitespace (bool): Whether to strip excess whitespace. Default is True.
     """
 
     # Remove directional punctuation
@@ -30,5 +31,9 @@ def standardize_transcript(text:str) -> str:
     text = text.replace("”", '"')
     text = text.replace("‘", "'")
     text = text.replace("“", '"')
+
+    # Remove excess whitespace
+    if strip_whitespace==True:
+        text = re.sub(r'\s+', ' ', text).strip()
 
     return text
